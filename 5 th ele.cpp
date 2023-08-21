41cfb8f85c1d570d90924913ab1fdbde809d27a5
surusaisaranya <125387475+surusaisaranya@users.noreply.github.com>
2023-08-21 05:31:58 -0600

    Add files via upload

---
+++ 5 th ele.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Define an array of elements
+    int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    
+    // Calculate the index of the 5th element (since array indices are 0-based)
+    int fifthIndex = 4;  // 5th element corresponds to index 4
+    
+    // Display the 5th element
+    printf("The 5th element is: %d\n", array[fifthIndex]);
+    
+    return 0;
+}
+
