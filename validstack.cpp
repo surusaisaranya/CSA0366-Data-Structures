c90ad6b649f36ef8bcb930c93c9e5154200efb96
surusaisaranya <125387475+surusaisaranya@users.noreply.github.com>
2023-08-21 05:43:34 -0600

    Add files via upload

---
+++ validstack.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+struct Stack {
+    int data[MAX_SIZE];
+    int top;
+};
+void initialize(struct Stack* stack) {
+    stack->top = -1;
+}
+int isEmpty(struct Stack* stack) {
+    return stack->top == -1;
+}
+int isFull(struct Stack* stack) {
+    return stack->top == MAX_SIZE - 1;
+}
+void push(struct Stack* stack, int value) {
+    if (isFull(stack)) {
+        printf("Stack overflow\n");
+        return;
+    }
+    stack->data[++stack->top] = value;
+}
+int pop(struct Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stack underflow\n");
+        return -1;
+    }
+    return stack->data[stack->top--];
+}
+int peek(struct Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    return stack->data[stack->top];
+}
+int main() {
+    struct Stack stack;
+    initialize(&stack);
+    push(&stack, 10);
+    push(&stack, 20);
+    push(&stack, 30);
+    printf("Top element: %d\n", peek(&stack));
+    printf("Popped element: %d\n", pop(&stack));
+    printf("Popped element: %d\n", pop(&stack));
+    printf("Popped element: %d\n", pop(&stack));
+    printf("Popped element: %d\n", pop(&stack));
+    return 0;
+}
+
